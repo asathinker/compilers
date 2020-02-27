6321a0d5c857efd01e9f8cfceef1f390bbe18dfc
asathinker <omtty@foxmail.com>
2020-02-27 16:33:37 +0800

    人人都可以学会编译原理-词法分析之四则运算解释器

--- src/asathinker/compilers/calculator/Calculator.java
+++ src/asathinker/compilers/calculator/Calculator.java
@@ -14,7 +14,41 @@ public class Calculator {
 	 * @return 四则运算结果
 	 */
 	public static double evaluate(AstNode ast) {
-		return 0;
+		// 常量节点
+		if (ast instanceof Constant) {
+			// 常量运算，字符串转数
+			return Double.valueOf(((Constant) ast).getValue().getText());
+		} else if (ast instanceof BinaryOp) {
+			// 二元运算节点
+			BinaryOp binaryOp = (BinaryOp) ast;
+			// 左操作数
+			AstNode left = binaryOp.getLeft();
+			// 右操作数
+			AstNode right = binaryOp.getRight();
+			if (binaryOp.getOperator().getType() == Token.Type.Plus) {
+				// 加法
+				return evaluate(left) + evaluate(right);
+			} else if (binaryOp.getOperator().getType() == Token.Type.Minus) {
+				// 减法
+				return evaluate(left) - evaluate(right);
+			} else if (binaryOp.getOperator().getType() == Token.Type.Star) {
+				// 乘法
+				return evaluate(left) * evaluate(right);
+			} else {
+				// 除法
+				return evaluate(left) / evaluate(right);
+			}
+		} else {
+			// 一元运算节点
+			UnaryOp unaryOp = (UnaryOp) ast;
+			// 操作数
+			AstNode operand = unaryOp.getOperand();
+			if (unaryOp.getOperator().getType() == Token.Type.Minus) {
+				// 取负运算
+				return -evaluate(operand);
+			}
+			return evaluate(operand);
+		}
 	}
 
 	public static void main(String[] args) {
